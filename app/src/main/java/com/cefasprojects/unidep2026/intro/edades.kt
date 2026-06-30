3f1c62aa29f0ae0a363046051518300a435a76c8
Pedro <salazar9_5@hotmail.com>
2026-06-30 12:41:34 -0700

    Tarea datos persona

---
+++ app/src/main/java/com/cefasprojects/unidep2026/intro/edades.kt
@@ -0,0 +1,10 @@
+package com.cefasprojects.unidep2026.intro
+
+fun main(){
+    //Se pediran los datos de una persona
+    //Nombre
+    //Genero
+    //Edad
+
+    //mostrar los datos de la persona
+}
